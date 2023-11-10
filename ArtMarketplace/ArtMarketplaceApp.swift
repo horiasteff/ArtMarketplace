d1507e371c44a60499f8033755540024d71540ea
horiamunteanu <munteanuhoria19@stud.ase.ro>
2023-11-10 15:18:18 +0200

    Initial Commit

---
+++ ArtMarketplace/ArtMarketplaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtMarketplaceApp.swift
+//  ArtMarketplace
+//
+//  Created by Horia Munteanu on 10.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct ArtMarketplaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
