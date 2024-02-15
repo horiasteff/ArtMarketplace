51dd70f6cac66603d3f1cc90a87b494b0bdb73d1
horiamunteanu <munteanuhoria19@stud.ase.ro>
2024-02-15 17:20:55 +0200

    Second commit

--- ArtMarketplace/Core/Authentication/ViewModel/AuthViewModel.swift
+++ ArtMarketplace/Core/Authentication/ViewModel/AuthViewModel.swift
@@ -15,11 +15,22 @@ class AuthViewModel: ObservableObject {
     @Published var currentUser: User?
     
     init(){
+        self.userSession = Auth.auth().currentUser
         
+        Task {
+            await fetchUser()
+        }
     }
     
     func singIn(withEmail email: String, password: String) async throws {
-        print("Sign in...")
+        do {
+            let result = try await Auth.auth().signIn(withEmail: email, password: password)
+            self.userSession = result.user
+            await fetchUser()
+            
+        }catch {
+            print("DEBUG: Failed to log in with error \(error.localizedDescription)")
+        }
     }
     
     func createUser(withEmail email: String, password: String, fullname: String) async throws {
@@ -29,13 +40,20 @@ class AuthViewModel: ObservableObject {
             let user = User(id: result.user.uid, fullname: fullname, email: email)
             let encodedUser = try Firestore.Encoder().encode(user)
             try await Firestore.firestore().collection("users").document(user.id).setData(encodedUser)
+            await fetchUser()
         }catch {
             print("Failed to create user with error \(error.localizedDescription)")
         }
     }
     
     func signOut(){
-        
+        do {
+            try Auth.auth().signOut()
+            self.userSession = nil
+            self.currentUser = nil
+        } catch {
+            print ("DEBUG: Failed to sign out with error \(error.localizedDescription)")
+        }
     }
     
     func deleteAccount(){
@@ -46,7 +64,9 @@ class AuthViewModel: ObservableObject {
         guard let uid = Auth.auth().currentUser?.uid else { return }
         
         guard let snapshot = try? await Firestore.firestore().collection("users").document(uid).getDocument() else { return }
-        //self.currentUser = snapshot.data(as: User.self)
+        self.currentUser = try? snapshot.data(as: User.self)
+        
+        print("Debug the current usier is \(String(describing: self.currentUser))")
     }
     
 }
