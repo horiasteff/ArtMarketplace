aa8104007823bd00ba019b84fd038f5762a9bdc9
Horia Stefan Munteanu <hmuntean@intralinks.com>
2024-05-23 14:18:52 +0300

    Customized add/withdraw money forms

--- ArtMarketplace/Components/AddMoneyFormView.swift
+++ ArtMarketplace/Components/AddMoneyFormView.swift
@@ -15,42 +15,62 @@ struct AddMoneyFormView: View {
     @Environment(\.presentationMode) var presentationMode
     
     var body: some View {
-        VStack {
-            TextField("Enter amount", text: $amount)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding()
-            
-            Button("Add Money") {
-                guard let amount = Double(amount), amount > 0 else {
-                    showToast = true
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                        withAnimation {
-                            showToast = false
+        ZStack{
+            LinearGradient(gradient: Gradient(colors: [Color("kPrimary").opacity(1), Color("kPrimary").opacity(0.2)]), startPoint: .top, endPoint: .bottom).ignoresSafeArea()
+            VStack {
+                TextField("   Enter amount", text: $amount)
+                    .frame(height: 50)
+                    .background(Color("kSecondary"))
+                    .cornerRadius(20)
+                    .padding()
+                
+                Button("Add Money") {
+                    guard let amount = Double(amount), amount > 0 else {
+                        showToast = true
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                            withAnimation {
+                                showToast = false
+                            }
                         }
+                        return
                     }
-                    return
+                    userWalletManager.updateWalletBalanceInFirestore(withAmount: amount)
+                    userWalletManager.recordTransaction(type: "Money Added", amount: amount)
+                    presentationMode.wrappedValue.dismiss()
                 }
-                userWalletManager.updateWalletBalanceInFirestore(withAmount: amount)
-                userWalletManager.recordTransaction(type: "Money Added", amount: amount)
-                presentationMode.wrappedValue.dismiss()
+                .frame(width: 150, height: 50)
+                .background(Color("kPrimary"))
+                .cornerRadius(20)
+                .foregroundColor(.white)
+                .padding()
             }
             .padding()
-        }
-        .padding()
-        .overlay(
-            VStack {
-                if showToast {
-                    Text("Please enter a valid amount")
-                        .foregroundColor(.white)
-                        .padding()
-                        .background(Color.red)
-                        .cornerRadius(10)
-                        .padding(.horizontal)
+            .overlay(
+                VStack {
+                    if showToast {
+                        Text("Please enter a valid amount")
+                            .foregroundColor(.white)
+                            .padding()
+                            .background(Color.red)
+                            .cornerRadius(10)
+                            .padding(.horizontal)
+                    }
+                    Spacer()
                 }
-                Spacer()
-            }
-            , alignment: .top
-        )
+                , alignment: .top
+            )
+        }
+    }
+}
+
+
+struct OvalTextFieldStyle: TextFieldStyle {
+    func _body(configuration: TextField<Self._Label>) -> some View {
+        configuration
+            .padding(10)
+            .background(LinearGradient(gradient: Gradient(colors: [Color.orange, Color.orange]), startPoint: .topLeading, endPoint: .bottomTrailing))
+            .cornerRadius(20)
+            .shadow(color: .gray, radius: 10)
     }
 }
 
--- ArtMarketplace/Components/WithdrawMoneyFormView.swift
+++ ArtMarketplace/Components/WithdrawMoneyFormView.swift
@@ -16,62 +16,71 @@ struct WithdrawMoneyFormView: View {
     @Environment(\.presentationMode) var presentationMode
     
     var body: some View {
-        VStack {
-            TextField("Enter amount", text: $amount)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
+        ZStack{
+            LinearGradient(gradient: Gradient(colors: [Color("kPrimary").opacity(1), Color("kPrimary").opacity(0.2)]), startPoint: .top, endPoint: .bottom).ignoresSafeArea()
+            VStack {
+                TextField("   Enter amount", text: $amount)
+                    .frame(height: 50)
+                    .background(Color("kSecondary"))
+                    .cornerRadius(20)
+                    .padding()
+                
+                Button("Withdraw Money") {
+                    guard let withdrawAmount = Double(amount), withdrawAmount > 0 else {
+                        showToast = true
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                            withAnimation {
+                                showToast = false
+                            }
+                        }
+                        return
+                    }
+                    
+                    guard withdrawAmount <= userWalletManager.walletBalance else {
+                        showToast2 = true
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                            withAnimation {
+                                showToast2 = false
+                            }
+                        }
+                        return
+                    }
+                    
+                    userWalletManager.withdrawWalletBalanceInFirestore(withAmount: withdrawAmount)
+                    userWalletManager.recordTransaction(type: "Money Withdrawed", amount: withdrawAmount)
+                    presentationMode.wrappedValue.dismiss()
+                }
+                .frame(width: 150, height: 50)
+                .background(Color("kPrimary"))
+                .cornerRadius(20)
+                .foregroundColor(.white)
                 .padding()
-            
-            Button("Withdraw Money") {
-                guard let withdrawAmount = Double(amount), withdrawAmount > 0 else {
-                    showToast = true
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                        withAnimation {
-                            showToast = false
-                        }
-                    }
-                    return
-                }
-                
-                guard withdrawAmount <= userWalletManager.walletBalance else {
-                    showToast2 = true
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                        withAnimation {
-                            showToast2 = false
-                        }
-                    }
-                    return
-                }
-                
-                userWalletManager.withdrawWalletBalanceInFirestore(withAmount: withdrawAmount)
-                userWalletManager.recordTransaction(type: "Money Withdrawed", amount: withdrawAmount)
-                presentationMode.wrappedValue.dismiss()
             }
             .padding()
+            .overlay(
+                VStack {
+                    if showToast {
+                        Text("Please enter a valid amount")
+                            .foregroundColor(.white)
+                            .padding()
+                            .background(Color.red)
+                            .cornerRadius(10)
+                            .padding(.horizontal)
+                    }
+                    if showToast2 {
+                        Text("There is no enough money to withdraw")
+                            .foregroundColor(.white)
+                            .padding()
+                            .background(Color.red)
+                            .cornerRadius(10)
+                            .padding(.horizontal)
+                    }
+                    
+                    Spacer()
+                }
+                , alignment: .top
+            )
         }
-        .padding()
-        .overlay(
-            VStack {
-                if showToast {
-                    Text("Please enter a valid amount")
-                        .foregroundColor(.white)
-                        .padding()
-                        .background(Color.red)
-                        .cornerRadius(10)
-                        .padding(.horizontal)
-                }
-                if showToast2 {
-                    Text("There is no enough money to withdraw")
-                        .foregroundColor(.white)
-                        .padding()
-                        .background(Color.red)
-                        .cornerRadius(10)
-                        .padding(.horizontal)
-                }
-                
-                Spacer()
-            }
-            , alignment: .top
-        )
     }
 }
 
